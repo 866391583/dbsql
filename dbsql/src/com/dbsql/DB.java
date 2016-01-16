edbd56764c8442f820d5029aa32c02459df882df
ubuntu <ubuntu@Lenovo-G505>
2016-01-16 18:46:03 +0800

    init

---
+++ dbsql/src/com/dbsql/DB.java
@@ -0,0 +1,20 @@
+package com.dbsql;
+
+public class DB {
+	String name;
+	int version;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getVersion() {
+		return version;
+	}
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+
+}
